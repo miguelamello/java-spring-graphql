cf080fa93918d36a7c56d1569586f092e32619f2
Miguel Angelo Mello] <miguelangelomello@gmail.com>
2023-05-26 11:28:38 -0300

    2023-05-26 11:28:38 by Miguel

---
+++ .history/src/main/java/com/example/spring/Measurement_20230526110950.java
@@ -0,0 +1,31 @@
+package com.example.spring;
+
+import java.util.Arrays;
+import java.util.List;
+
+public record Measurement (
+    String id,
+    String meter,
+    String value,
+    String timestamp
+) {
+
+    private static List<Measurement> meters = Arrays.asList(
+      new Measurement("1", "Evenrude ACD-38", "Sector 7G", "2022-03-06T00:00:00Z", ""),
+      new Measurement("2", "Mercury JFH-34", "Sector 1K", "2021-07-02T00:00:00Z", ""),
+      new Measurement("3", "Jhonson TT64", "Sector 3B", "2022-05-03T00:00:00Z", "2022-07-28T00:00:00Z", "")
+    );
+
+    public static Measurement getById(String id) {
+      return meters.stream()
+      .filter(meter -> meter.id().equals(id))
+      .findFirst()
+      .orElse(null);
+    }
+
+    public static List<Measurement> getAll() {
+      return meters;
+    }
+    
+}
+
---
+++ .history/src/main/java/com/example/spring/Measurement_20230526111128.java
@@ -0,0 +1,31 @@
+package com.example.spring;
+
+import java.util.Arrays;
+import java.util.List;
+
+public record Measurement (
+    String id,
+    String meter,
+    String value,
+    String timestamp
+) {
+
+    private static List<Measurement> meters = Arrays.asList(
+      new Measurement("1", "1", "10K", "2022-03-06T00:00:00Z"),
+      new Measurement("2", "1", "Sector 1K", "2021-07-02T00:00:00Z"),
+      new Measurement("3", "1", "Sector 3B", "2022-07-28T00:00:00Z")
+    );
+
+    public static Measurement getById(String id) {
+      return meters.stream()
+      .filter(meter -> meter.id().equals(id))
+      .findFirst()
+      .orElse(null);
+    }
+
+    public static List<Measurement> getAll() {
+      return meters;
+    }
+    
+}
+
